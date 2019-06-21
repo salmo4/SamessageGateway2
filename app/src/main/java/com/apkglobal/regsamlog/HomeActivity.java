6e7219b56bce875eb1d14cf58868fdf625747d81
salmo4 <samy_gui95@yahoo.com>
2019-06-21 23:14:31 +0300

    Initial commit

---
+++ app/src/main/java/com/apkglobal/regsamlog/HomeActivity.java
@@ -0,0 +1,94 @@
+package com.apkglobal.regsamlog;
+
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btn_trimite, btnLogout;
+    EditText sam_mobil, sam_mesaj;
+
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn_trimite=findViewById(R.id.btn_trimite);
+        sam_mesaj=findViewById(R.id.sam_mesaj);
+        sam_mobil=findViewById(R.id.sam_mobil);
+        btn_trimite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // copiem aici codul Java din textlocal de trimitere SMS
+                // tastam alt+Enter pentru a importa clasa
+                try {
+                    // Construct data
+                    // key este valoare fixa
+                    String apiKey = "apikey=" + "zHbjyCm0JwY-6WmGbU5EhHOjL7KeJ2sC44F5tvCsFq";
+                    // mesajul intotdeauna se poate schimba apeland sam_mesaj
+                    String message = "&message=" + sam_mesaj.getText().toString();
+                    // fiind cont demo nu se poate schimba numele de id a user-ului
+                    String sender = "&sender=" + "Jims Autos";
+                    // chiar daca e cont demo se poate schimba numarul de mobil al receptorului oricand
+                    String numbers = "&numbers=" + sam_mobil.getText().toString();
+
+                    // Send data
+                    HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
+                    String data = apiKey + numbers + message + sender;
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+                    conn.getOutputStream().write(data.getBytes("UTF-8"));
+                    final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    final StringBuffer stringBuffer = new StringBuffer();
+                    String line;
+                    while ((line = rd.readLine()) != null) {
+                        Toast.makeText(HomeActivity.this,
+                                line.toString() , Toast.LENGTH_SHORT).show();
+                    }
+                    rd.close();
+
+
+                } catch (Exception e) {
+                    Toast.makeText(HomeActivity.this,
+                            e.toString() , Toast.LENGTH_SHORT).show();
+
+                }
+
+
+
+            }
+        });
+        // codul pt metoda on click
+        StrictMode.ThreadPolicy st=new StrictMode.ThreadPolicy.Builder().build();
+        StrictMode.setThreadPolicy(st);
+
+        // se poate rula aplicatia pe un telefon mobil
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/apkglobal/regsamlog/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.apkglobal.regsamlog;
+
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText emailId, password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignIn = findViewById(R.id.button2);
+        tvSignUp = findViewById(R.id.textView);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(LoginActivity.this,"Esti logat! Poti trimite SMS-uri",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Logheaza-te",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Introduce-ti adresa de email");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Introduce-ti parola");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Campurile sunt goale!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this,"Eroare de logare, Logheaza-te din nou!",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(LoginActivity.this,HomeActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Eroare!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intSignUp = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intSignUp);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+}
